cf740bdee18cf19db12aff192a94556ee84b874c
Andrew Lombardi <alombardi@twitter.com>
2019-04-13 08:36:15 -0700

    updates for ConFoo 2019

--- terraform/elasticache.tf
+++ terraform/elasticache.tf
@@ -4,7 +4,7 @@ resource "aws_elasticache_cluster" "api_cache" {
   node_type            = "cache.t2.micro"
   port                 = 6379
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis3.2"
+  parameter_group_name = "default.redis5.0"
   subnet_group_name    = "${aws_elasticache_subnet_group.api_cache.name}"
   security_group_ids   = ["${aws_security_group.j2d_elasticache_redis.id}"]
 
--- terraform/identity.tf
+++ terraform/identity.tf
@@ -1,6 +1,6 @@
 
-data "aws_caller_identity" "current" {}
+#data "aws_caller_identity" "current" {}
 
-data "aws_region" "current" {
-  current = true
-}
+#data "aws_region" "current" {
+#  current = true
+#}
